278cc95490619ec41e855476a48201f3becb03fe
AdrenalinJunkie <woollc007@gmail.com>
2019-09-23 09:01:19 +0100

    generated pages + header

--- skull-app/src/app/app.module.ts
+++ skull-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SkullHeaderComponent } from './skull-header/skull-header.component';
+import { GamePageComponent } from './game-page/game-page.component';
+import { RulePageComponent } from './rule-page/rule-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SkullHeaderComponent,
+    GamePageComponent,
+    RulePageComponent
   ],
   imports: [
     BrowserModule,
---
+++ skull-app/src/app/skull-header/skull-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-skull-header',
+  templateUrl: './skull-header.component.html',
+  styleUrls: ['./skull-header.component.scss']
+})
+export class SkullHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
